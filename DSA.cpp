aede43c59ef8beb8463bcd241f4bc77f66904481
QuantumCoder02 <techie.mohsen@gmail.com>
2026-02-12 12:41:16 +0100

    Add files via upload

---
+++ DSA.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "clsQueueLine.h"
+
+using namespace std;
+
+int main()
+{
+
+	clsQueueLine Q1 = clsQueueLine("First Class", 3);
+	Q1.IssueTicket();
+
+	Q1.PrintInfo();
+
+
+}
+
+
+
+
